48de375cb8c5729efe98e36fb00b3031cdb06bf1
Carlos Roberto Velasquez <carlosrobertovelasquez@gmail.com>
2023-09-24 18:36:54 -0600

    movemos el archivo indice

--- api/index.js
+++ api/index.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { dbConnect } = require("./api/config/mssql");
+const { dbConnect } = require("./config/mssql");
 app.use(cors());
 const port = process.env.PORT || 3000;
 //Aqui invocamos a las rutas
-app.use("/api/v1", require("./api/routes"));
+app.use("/api/v1", require("./routes"));
 app.listen(port, () => {
   console.log("Tu app esta lista por http://localhost:" + port);
 });
